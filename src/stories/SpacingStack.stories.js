eaea0ee7128a64d062b61baeaa74f3bf64e4e23e
Shubham-compose-tech <shubham@compose.co.in>
2024-03-27 09:37:06 +0530

    configured storybook

---
+++ src/stories/SpacingStack.stories.js
@@ -0,0 +1,13 @@
+import SpacingsStack from "./SpacingStack";
+
+const meta = {
+  title: "Example/SpacingsStack",
+  component: SpacingsStack,
+} ;
+export default meta;
+
+export const Secondary = {
+  args: {
+    label: "Spacing Stack",
+  },
+};
