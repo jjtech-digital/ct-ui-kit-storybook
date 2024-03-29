4aba0e8ae9fffc38cdd9644e91e8dfd0854b630d
Shubham-compose-tech <shubham@compose.co.in>
2024-03-29 15:58:50 +0530

    feat: created loader story

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoadingSpinner from "@commercetools-uikit/loading-spinner";
+
+const LoadingCt = () => {
+  return <LoadingSpinner size="s">Loading text</LoadingSpinner>;
+};
+
+export default LoadingCt;
