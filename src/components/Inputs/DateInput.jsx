ca44845eae3f669144ade97d82c9ec85c2c37d81
Shubham-compose-tech <shubham@compose.co.in>
2024-04-01 16:45:50 +0530

    feat: created input story

---
+++ src/components/Inputs/DateInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DateInput from "@commercetools-uikit/date-input";
+import { IntlProvider } from "react-intl";
+const DateInputCt = ({ placeholder, value, onChange }) => {
+  return (
+    <IntlProvider locale="en">
+      <DateInput placeholder={placeholder} value={value} onChange={onChange} />
+    </IntlProvider>
+  );
+};
+
+export default DateInputCt;
---
+++ src/components/Inputs/Input.stories.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import InputCt from "./Input";
+import DateInputCt from "./DateInput";
+
+export default {
+  title: "Example/Inputs",
+};
+export const CheckboxInput = () => {
+  const [checked, setChecked] = useState({
+    checkbox1: true,
+    checkbox2: false,
+    errCheckbox1: true,
+    errCheckbox2: false,
+  });
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+      <div style={{ display: "flex", gap: "6px" }}>
+        <p style={{ width: "100px" }}>Default</p>
+        <div style={{ display: "flex", gap: "6px" }}>
+          <InputCt
+            isChecked={checked.checkbox1}
+            onChange={() =>
+              setChecked((prev) => ({ ...prev, checkbox1: !prev.checkbox1 }))
+            }
+            label={"This is a label"}
+          />
+          <InputCt
+            isChecked={checked.checkbox2}
+            onChange={() =>
+              setChecked((prev) => ({ ...prev, checkbox2: !prev.checkbox2 }))
+            }
+            label={"This is a label"}
+          />
+          <InputCt label={"This is a label"} isIndeterminate={true} />
+        </div>
+      </div>
+      <div style={{ display: "flex", gap: "6px" }}>
+        <p style={{ width: "100px" }}>Disabled</p>
+        <div style={{ display: "flex", gap: "6px" }}>
+          <InputCt
+            isChecked={true}
+            isDisabled={true}
+            label={"This is a label"}
+          />
+          <InputCt
+            isChecked={false}
+            isDisabled={true}
+            label={"This is a label"}
+          />
+          <InputCt
+            isDisabled={true}
+            label={"This is a label"}
+            isIndeterminate={true}
+          />
+        </div>
+      </div>
+      <div style={{ display: "flex", gap: "6px" }}>
+        <p style={{ width: "100px" }}>Read only</p>
+        <div style={{ display: "flex", gap: "6px" }}>
+          <InputCt
+            isChecked={true}
+            isReadOnly={true}
+            label={"This is a label"}
+          />
+          <InputCt
+            isChecked={false}
+            isReadOnly={true}
+            label={"This is a label"}
+          />
+          <InputCt
+            isReadOnly={true}
+            label={"This is a label"}
+            isIndeterminate={true}
+          />
+        </div>
+      </div>
+      <div style={{ display: "flex", gap: "6px" }}>
+        <p style={{ width: "100px" }}>Error</p>
+        <div style={{ display: "flex", gap: "6px" }}>
+          <InputCt
+            isChecked={checked.errCheckbox1}
+            onChange={() =>
+              setChecked((prev) => ({
+                ...prev,
+                errCheckbox1: !prev.errCheckbox1,
+              }))
+            }
+            hasError={true}
+            label={"This is a label"}
+          />
+          <InputCt
+            isChecked={checked.errCheckbox2}
+            onChange={() =>
+              setChecked((prev) => ({
+                ...prev,
+                errCheckbox2: !prev.errCheckbox2,
+              }))
+            }
+            hasError={true}
+            label={"This is a label"}
+          />
+          <InputCt
+            hasError={true}
+            label={"This is a label"}
+            isIndeterminate={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const DateInput = () => {
+  return (
+    <div style={{ width: "40%" }}>
+      <DateInputCt
+        placeholder="Select a date..."
+        value="2017-12-31"
+        onChange={() => {}}
+      />
+    </div>
+  );
+};
