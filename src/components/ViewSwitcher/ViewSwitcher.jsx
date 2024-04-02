197b424db1020d7178c8531980b09cb8763de73f
Shubham-compose-tech <shubham@compose.co.in>
2024-04-02 12:44:09 +0530

    fix: refactor viewSwitcher story code

--- src/components/ViewSwitcher/ViewSwitcher.jsx
+++ src/components/ViewSwitcher/ViewSwitcher.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ViewSwitcher from "@commercetools-uikit/view-switcher";
 
-export const ViewwSwitcher = () => {
+const ViewSwitcherCt = ({ buttonsCount, children, iconsOnly }) => {
   return (
-    <ViewSwitcher.Group
-      defaultSelected="button 2"
-      onChange={(value) => console.log(value)}
-    >
-      <ViewSwitcher.Button isDisabled value="button 1">
-        View 1
-      </ViewSwitcher.Button>
-      <ViewSwitcher.Button value="button 2">View 2</ViewSwitcher.Button>
-      <ViewSwitcher.Button value="button 3">View 3</ViewSwitcher.Button>
-      <ViewSwitcher.Button value="button 4">View 4</ViewSwitcher.Button>
+    <ViewSwitcher.Group defaultSelected="button 2">
+      {[...Array(buttonsCount).keys()].map((index) => {
+        const i = index + 1;
+        const viewSwitcherButton = `Button ${i}`;
+        return (
+          <ViewSwitcher.Button key={i} value={viewSwitcherButton}>
+            {!iconsOnly && children} {`View ${i}`}
+          </ViewSwitcher.Button>
+        );
+      })}
     </ViewSwitcher.Group>
   );
 };
-
-
+export default ViewSwitcherCt;
