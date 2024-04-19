db15cb8f3411c9dd05b386ff00cd19bf71706240
chanchal013 <chanchal@compose.co.in>
2024-04-19 11:43:48 +0530

    update: add checkbox

--- src/components/CtGrid/BasicTable.jsx
+++ src/components/CtGrid/BasicTable.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 export const BasicTable = ({ rows, cols }) => {
-  return <DataGrid rows={rows} columns={cols} hideFooterPagination />;
+  return (
+    <DataGrid
+      sx={{
+        fontFamily: "Inter",
+        "& .Mui-checked": { color: "#101050 !important" },
+        "& .MuiDataGrid-row.Mui-selected": {
+          backgroundColor: `rgba(211, 204, 255, 0.3)!important`,
+        },
+      }}
+      rows={rows}
+      columns={cols}
+      hideFooterPagination
+      checkboxSelection
+    />
+  );
 };
